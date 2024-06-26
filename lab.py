7799a6b0325fbe2f146cc4584ad3858ac01b5a4b
zikrikn <zikrikn@student.telkomuniversity.ac.id>
2024-06-26 18:05:25 +0700

    changed to final

--- lab.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-loaded_array = np.load("/home/zikri/Documents/TA/FKI-github/emotion-recognition-smartwatch/deep_learning_data/mo_ew2_accdata_21_10_139-1336_x.npy")
-
-print("hasilnya: ", loaded_array)
